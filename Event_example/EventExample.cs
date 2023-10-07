453b02a9f35f657db095d69119a0ede3847c3654
Matthew D Hakin <mdhakin@alaska.edu>
2023-10-07 09:44:46 -0800

    Add project files.

---
+++ Event_example/EventExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Event_example
+{
+    public class EventExample
+    {
+        public delegate void DataReceived(object sender, EventArgs e, byte[] data);
+
+        public event DataReceived DataReceivedEvent;
+
+        /*
+        ExampleEvent.DataReceivedEvent += DataReceivedEventHandlerMethod;
+        private void DataReceivedEventHandlerMethod(object sender, EventArgs e, byte[] data)
+        {
+        }
+         */
+        public EventExample()
+        {
+            
+        }
+
+        public void testEvent()
+        {
+            byte[] bytes = { 1, 2, 3 };
+            EventArgs args = new EventArgs();
+            DataReceivedEvent?.Invoke(this, args, bytes);
+        }
+    }
+}
---
+++ Event_example/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Event_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        EventExample ExampleEvent = new EventExample();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ExampleEvent.DataReceivedEvent += DataReceivedEventHandlerMethod;
+        }
+
+        private void DataReceivedEventHandlerMethod(object sender, EventArgs e, byte[] data)
+        {
+            MessageBox.Show(data[0].ToString()+ data[2].ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExampleEvent.testEvent();
+        }
+    }
+}
